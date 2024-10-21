79ada0ead13c84ecfa6033bb4db316409c9270cd
rjmasache <royerjmasache@gmail.com>
2024-10-21 16:29:40 -0500

    Adición de fuente tipográfica y modificación de espaciado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,27 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Lato } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const lato = Lato({
+    weight: ["400"],
+    subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: "Pinecone Assistant Sample App",
-  description: "Connect a chat interface to an existing Pinecone Assistant",
+    title: "Pinecone Assistant Sample App",
+    description: "Connect a chat interface to an existing Pinecone Assistant",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={lato.className}>{children}</body>
+        </html>
+    );
 }
